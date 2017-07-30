60e98d6ff01df66de6894786585ce4e09230edef
riley <riley.martine@protonmail.com>
2017-07-30 13:04:21 -0400

    Add comments

--- pyfortune.py
+++ pyfortune.py
@@ -6,11 +6,13 @@ FORTUNES_FILES = ['myfortunes.txt']
 SCRIPT_PATH = os.path.dirname(os.path.realpath(sys.argv[0]))
 FF_PATHS = [os.path.join(SCRIPT_PATH, file_path) for file_path in FORTUNES_FILES] 
 
+# Check that all of the files are there
 for file_path in FF_PATHS:
   if not os.path.isfile(file_path):
       print("Fortunes file `{}` not found! Put file in `{}`".format(file_path,  SCRIPT_PATH))
       sys.exit(1)
 
+# Read in all database files
 FF_CONTENTS = ""
 for file_path in FF_PATHS:
   try:
@@ -19,17 +21,20 @@ for file_path in FF_PATHS:
       print("Cannot open fortunes file at " + file_path)
       sys.exit(1)
 
+# Pull fortunes out of files
 FORTUNES = FF_CONTENTS.split("%\n")
 FORTUNES_NO_NEWLINES = list(map(lambda x: x.strip(), FORTUNES))
 
 if len(FORTUNES_NO_NEWLINES) < 1:
     print("Add more fortunes!")
 
+# Get the randomest random we can random
 try:
     r = random.SystemRandom()
 except:
     r = random
 
+# Select a random fortune from all the fortunes
 YOUR_FORTUNE = FORTUNES_NO_NEWLINES[r.randint(0, len(FORTUNES_NO_NEWLINES) - 1)]
 
 print(YOUR_FORTUNE)
