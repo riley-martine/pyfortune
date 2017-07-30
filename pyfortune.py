55decca0687dbca0876ca448a1d0ec787a41492e
riley <riley.martine@protonmail.com>
2017-07-30 13:01:15 -0400

    Add support for multiple fortune files

--- pyfortune.py
+++ pyfortune.py
@@ -2,19 +2,22 @@ import os
 import sys
 import random
 
-FORTUNES_FILE = 'myfortunes.txt'
+FORTUNES_FILES = ['myfortunes.txt']
 SCRIPT_PATH = os.path.dirname(os.path.realpath(sys.argv[0]))
-FF_PATH = os.path.join(SCRIPT_PATH, FORTUNES_FILE)
+FF_PATHS = [os.path.join(SCRIPT_PATH, file_path) for file_path in FORTUNES_FILES] 
 
-if not os.path.isfile(FF_PATH):
-    print("Fortunes file not found! Put file in " + SCRIPT_PATH)
-    sys.exit(1)
+for file_path in FF_PATHS:
+  if not os.path.isfile(file_path):
+      print("Fortunes file `{}` not found! Put file in `{}`".format(file_path,  SCRIPT_PATH))
+      sys.exit(1)
 
-try:
-    FF_CONTENTS = open(FF_PATH, 'r').read()
-except IOError as er:
-    print("Cannot open fortunes file at " + FF_PATH)
-    sys.exit(1)
+FF_CONTENTS = ""
+for file_path in FF_PATHS:
+  try:
+      FF_CONTENTS += open(file_path, 'r').read()
+  except IOError as er:
+      print("Cannot open fortunes file at " + file_path)
+      sys.exit(1)
 
 FORTUNES = FF_CONTENTS.split("%\n")
 FORTUNES_NO_NEWLINES = list(map(lambda x: x.strip(), FORTUNES))
