6d9cf195ff0f736d5a8448c936f8104f52f0c248
Riley Martine <riley.martine@protonmail.com>
2017-10-12 09:44:03 -0400

    Add argparse and short/long opts

--- pyfortune.py
+++ pyfortune.py
@@ -1,13 +1,19 @@
 import os
 import sys
 import random
+import argparse
 
 FORTUNES_FILES = ['myfortunes.txt']
 SCRIPT_PATH = os.path.dirname(os.path.realpath(sys.argv[0]))
 FF_PATHS = [os.path.join(SCRIPT_PATH, file_path) for file_path in FORTUNES_FILES] 
 
+# Get the randomest random we can random
+try:
+  r = random.SystemRandom()
+except:
+  r = random
 
-def read_files(paths):
+def read_fortunes(paths):
   contents = ""
   for file_path in paths:
     # Check that all of the files are there
@@ -20,25 +26,38 @@ def read_files(paths):
     except IOError as er:
       print("Cannot open fortunes file at " + file_path)
       sys.exit(1)
-  return contents
+
+  fortunes = contents.split("%\n")
+  fortunes_no_newlines = [x.strip() for x in fortunes]
+  return fortunes_no_newlines
+
+def randfortune(fortunes):
+   return r.choice(fortunes)
 
 
-# Pull fortunes out of files
-CONTENTS = read_files(FF_PATHS)
-FORTUNES = CONTENTS.split("%\n")
-FORTUNES_NO_NEWLINES = [x.strip() for x in FORTUNES]
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Python implementation of `fortune` program")
+  parser.add_argument("-s", "--short", help="Print a fortune at most 160 characters in length", action='store_true')
+  parser.add_argument("-l", "--long", help="Print a fortune at least 160 characters in length", action='store_true')
+  args = parser.parse_args()
+  # print(args)
 
-if len(FORTUNES_NO_NEWLINES) < 1:
-  print("Add more fortunes!")
 
-# Get the randomest random we can random
-try:
-  r = random.SystemRandom()
-except:
-  r = random
 
-# Select a random fortune from all the fortunes
-YOUR_FORTUNE = FORTUNES_NO_NEWLINES[r.randint(0, len(FORTUNES_NO_NEWLINES) - 1)]
+  # Pull fortunes out of files
+  FORTUNES = read_fortunes(FF_PATHS)
+  
+  if args.short:
+    FORTUNES = list(filter(lambda x: len(x) <= 160, FORTUNES))
+  if args.long:
+    FORTUNES = list(filter(lambda x: len(x) >= 160, FORTUNES))
+    
 
-print(YOUR_FORTUNE)
+  if len(FORTUNES) < 1:
+    print("Add more fortunes!")
+    sys.exit(1)
+  
+  YOUR_FORTUNE = randfortune(FORTUNES)
+  
+  print(YOUR_FORTUNE)
 
